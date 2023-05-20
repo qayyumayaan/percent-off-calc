df561bc525a71834eed8246833a5a494e84e4f2f
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-20 14:57:56 -0400

    Add tax button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # percent-off-calc
-A simple calculator to let users calculate the percent off on a product. 
+A simple calculator to let users calculate the percent off on a product. The formula used is straightforward, just 
+`Final = Price * ( 1 - %off / 100)`
--- calc.js
+++ calc.js
@@ -4,21 +4,25 @@ new Vue({
         selectedCalculation: null,
         inputPrice: 0,
         inputPercentOff: 0,
+        inputTaxRate: 6.625,
         output: null,
         outputType: null,
     },
     methods: {
         calculate() {
-        let price = parseFloat(this.inputPrice);
-        let percentOff = parseFloat(this.inputPercentOff);
-
-        this.output = price * (1 - percentOff / 100);
-        this.outputType = "Final price"
-
-        if (isNaN(this.output)) {
-            this.output = 0;
-        }
+            let price = parseFloat(this.inputPrice);
+            let percentOff = parseFloat(this.inputPercentOff);
+            let taxRate = parseFloat(this.inputTaxRate);
         
+            let priceAfterDiscount = price * (1 - percentOff / 100);
+            let taxAmount = priceAfterDiscount * (taxRate / 100);
+        
+            this.output = priceAfterDiscount + taxAmount;
+            this.outputType = "Final price";
+        
+            if (isNaN(this.output)) {
+                this.output = 0;
+            }
         },
 
         reset() {
